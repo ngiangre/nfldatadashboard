c1b2abc83803f3b068511d437c2fd618cd442a21
Nick Giangreco <nick.giangreco@gmail.com>
2023-12-31 06:41:52 -0500

    added modules in homepage

--- R/mod_homepage.R
+++ R/mod_homepage.R
@@ -11,9 +11,9 @@ mod_homepage_ui <- function(id){
   ns <- NS(id)
   bslib::page_navbar(
       underline = TRUE,
-      bslib::nav_panel(title="QB"),
-      bslib::nav_panel(title="WR"),
-      bslib::nav_panel(title="RB"),
+      bslib::nav_panel(title="QB",mod_position_plots_ui(ns("qb_plots_1"))),
+      bslib::nav_panel(title="WR",mod_position_plots_ui(ns("wr_plots_1"))),
+      bslib::nav_panel(title="RB",mod_position_plots_ui(ns("rb_plots_1"))),
       title = "nfldashboard"
   )
 }
@@ -25,6 +25,9 @@ mod_homepage_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
     data_obj <- data_object$new()
+    mod_position_plots_server("qb_plots_1",data_obj,ptype="qb")
+    mod_position_plots_server("wr_plots_1",data_obj,ptype="wr")
+    mod_position_plots_server("rb_plots_1",data_obj,ptype="rb")
   })
 }
 
