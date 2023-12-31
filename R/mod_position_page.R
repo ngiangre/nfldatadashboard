d98b97c2080453f3bae22986af72d044bcc28ba9
Nick Giangreco <nick.giangreco@gmail.com>
2023-12-31 12:16:37 -0500

    added data extraction for module

--- R/mod_position_page.R
+++ R/mod_position_page.R
@@ -34,6 +34,34 @@ mod_position_page_server <- function(id,data_obj,ptype = c('qb','wr','rb')){
   ptype <- match.arg(ptype)
   moduleServer( id, function(input, output, session){
     ns <- session$ns
+    alldat_sw <-
+        data_obj$get_position_season_data(
+            ptype,
+            'sw'
+        ) |>
+        dplyr::select(
+            dplyr::all_of(c("player_display_name","team_abbr",'week',
+                            data_obj$get_position_vars(ptype)))
+        ) |>
+        tidyr::pivot_longer(
+            cols = data_obj$get_position_vars(ptype),
+            names_to = "statistic"
+        )
+    subdat_sw <- reactive({alldat_sw |> filter(player_display_name %in% input$player)})
+    alldat_sa <-
+        data_obj$get_position_season_data(
+            ptype,
+            'sa'
+        ) |>
+        dplyr::select(
+            dplyr::all_of(c("player_display_name","team_abbr",
+                            data_obj$get_position_vars(ptype)))
+        ) |>
+        tidyr::pivot_longer(
+            cols = data_obj$get_position_vars(ptype),
+            names_to = "statistic"
+        )
+    subdat_sa <- reactive({alldat_sa |> filter(player_display_name %in% input$player)})
     #https://stackoverflow.com/questions/73716725/is-there-a-way-to-display-html-inside-a-selectinput-in-an-r-shiny-app
     renderSelectizeUI <-
         I("{
