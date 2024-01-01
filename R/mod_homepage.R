2519e68517345bcb2659aba488ca95e308bdd69f
Nick Giangreco <nick.giangreco@gmail.com>
2024-01-01 14:27:31 -0500

    add github repo link to navbar

--- R/mod_homepage.R
+++ R/mod_homepage.R
@@ -7,9 +7,15 @@
 #' @noRd
 #'
 #' @importFrom shiny NS tagList
-#' @importFrom bslib page_navbar bs_theme font_google nav_panel tooltip
+#' @importFrom bslib page_navbar bs_theme font_google nav_panel tooltip nav_spacer nav_item
 mod_homepage_ui <- function(id){
   ns <- NS(id)
+  link_github <- tags$a(
+      shiny::icon("github"), "nfldatadashboard",
+      href = "https://github.com/ngiangre/nfldatadashboard",
+      target = "_blank"
+  )
+
   bslib::page_navbar(
       theme = bslib::bs_theme(version = 5,base_font = bslib::font_google("Roboto"), font_scale = NULL,
                               `enable-gradients` = TRUE, preset = "zephyr"),
@@ -17,6 +23,8 @@ mod_homepage_ui <- function(id){
       bslib::nav_panel(title="Quarterbacks",mod_position_page_ui(ns("qb_page_1"))),
       bslib::nav_panel(title="Wide Receivers",mod_position_page_ui(ns("wr_page_1"))),
       bslib::nav_panel(title="Running Backs",mod_position_page_ui(ns("rb_page_1"))),
+      bslib::nav_spacer(),
+      bslib::nav_item(link_github),
       title = bslib::tooltip(list("nfldatadashboard",bsicons::bs_icon("info-circle")),
                              "Dashboard Goal: Visualize NFL player's performance.",
                              "1) Select players and performance indicators.",
